1d8c0235472c0c7c6dbcd557ecb46842fc78e4c1
T-NhanNguyen <n.940449496@gmail.com>
2020-01-13 23:41:17 -0800

    navpath and movement

---
+++ main/Assets/Scripts/Unit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Unit : MonoBehaviour
+{
+    // short cut UnitSelect file as selectunit
+    UnitSelection selectunit;
+    UnityEngine.AI.NavMeshAgent agent;
+    
+    RaycastHit hit;
+
+    void Start() {
+        selectunit = GameObject.Find("UnitManager").GetComponent<UnitSelection>();
+        agent = this.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (selectunit) {
+            if (selectunit.UnitSelect == this.gameObject || selectunit.groupSelect.Contains(this.gameObject)) {
+                Debug.Log("Got through the gameobject!");
+                if (Input.GetMouseButtonDown(1)) {
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                        if (hit.transform.tag == "Floor") {
+                            agent.destination = hit.point;
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
---
+++ main/Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelection : MonoBehaviour
+{
+    public GameObject UnitSelect;
+    RaycastHit hit;
+    public List<GameObject> groupSelect = new List<GameObject>();
+
+    void Update(){
+        if (UnitSelect == null) {
+            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift)) {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                    if (hit.transform.tag == "SelectableUnit") {
+                        UnitSelect = hit.transform.gameObject;
+                        UnitSelect.transform.Find("Marker").gameObject.SetActive(true);
+                    }
+                }
+            }
+        }
+        else {
+            if (Input.GetMouseButtonDown(0) && !Input.GetKey(KeyCode.LeftShift)) {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)){
+                    if (hit.transform.tag == "SelectableUnit") {
+                        UnitSelect.transform.Find("Marker").gameObject.SetActive(false);
+                        UnitSelect = hit.transform.gameObject;
+                        UnitSelect.transform.Find("Marker").gameObject.SetActive(true);
+                    }
+                }
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftShift)) {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)){
+                if (hit.transform.tag == "SelectableUnit") {
+                    if (UnitSelect != null){
+                        groupSelect.Add(UnitSelect);
+                        UnitSelect = null;
+                        
+                    }
+                    groupSelect.Add(hit.transform.gameObject);
+
+                    for(int i = 0; i < groupSelect.Count; i++){
+                        groupSelect[i].transform.Find("Marker").gameObject.SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
